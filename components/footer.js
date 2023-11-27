51718743fd74363e434276481f56b4b2b80770b4
Vaiva <vstau@tuta.io>
2023-11-27 03:42:25 -0600

    sdfdf

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import Container from "./container";
-import { EXAMPLE_PATH } from "../lib/constants";
+import Button from "./button";
 
 export default function Footer() {
   return (
@@ -10,12 +10,13 @@ export default function Footer() {
             Hi, Doug.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
+            {/* <a
               href="https://tetr.io/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Play Tetris
-            </a>
+            </a> */}
+            <Button text="Play Tetris" href="https://tetr.io/" />
             <a
               href={`https://www.youtube.com/watch?v=9d4-OxBy16Y`}
               className="mx-3 font-bold hover:underline"
