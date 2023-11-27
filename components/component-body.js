50760a223bfb84eba2d0c663e1042444728ec435
Vaiva <vstau@tuta.io>
2023-11-27 04:06:36 -0600

    sdfdf

--- components/component-body.js
+++ components/component-body.js
@@ -1,15 +1,37 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coy } from "react-syntax-highlighter/dist/cjs/styles/prism"; // Import the CommonJS version
+import { coy } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import SectionSeparator from "./section-separator";
+import Button from "./button";
 
 export default function ComponentBody({ content, code }) {
-  console.log(content);
+  const handleCopyClick = () => {
+    // Create a textarea element to store the code
+    const textarea = document.createElement("textarea");
+    textarea.value = content.code;
+
+    // Append the textarea to the document
+    document.body.appendChild(textarea);
+
+    // Select the text in the textarea
+    textarea.select();
+    textarea.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+
+    // Remove the textarea from the document
+    document.body.removeChild(textarea);
+  };
+
   return (
-    <div className={`max-w-2xl mx-auto`}>
+    <div className={`max-w-2xl mx-auto flex flex-col gap-8`}>
       {code ? (
-        <SyntaxHighlighter language="jsx" style={coy}>
-          {content.code}
-        </SyntaxHighlighter>
+        <>
+          <Button text="Copy" onClick={handleCopyClick} />
+          <SyntaxHighlighter language="jsx" style={coy}>
+            {content.code}
+          </SyntaxHighlighter>
+        </>
       ) : (
         <>
           <p>{content}</p>
