ed563361634097b67aa0bce22ba641cd54ebb715
Vaiva <vstau@tuta.io>
2023-11-27 03:51:42 -0600

    sdfdsf

--- components/post-plug.js
+++ components/post-plug.js
@@ -26,7 +26,26 @@ export default function PostPlug({
       <div className="mb-4 text-lg">
         <Date dateString={date} />
       </div>
-      <p className="mb-4 text-lg leading-relaxed">{excerpt}</p>
+      <div className="mb-4 text-lg leading-relaxed">
+        {excerpt ? (
+          <p>{excerpt}</p>
+        ) : (
+          <p className="text-my-blue text-sm">
+            When I heard the learn’d astronomer,
+            <br />
+            When the proofs, the figures, were ranged in columns before me,
+            <br /> When I was shown the charts and diagrams, to add, divide, and
+            measure them,
+            <br /> When I sitting heard the astronomer where he lectured with
+            much applause in the lecture-room,
+            <br /> How soon unaccountable I became tired and sick,
+            <br /> Till rising and gliding out I wander’d off by myself,
+            <br /> In the mystical moist night-air, and from time to time,
+            <br />
+            Look’d up in perfect silence at the stars.
+          </p>
+        )}
+      </div>
       {author && <Avatar name={author.name} picture={author.picture} />}
     </div>
   );
